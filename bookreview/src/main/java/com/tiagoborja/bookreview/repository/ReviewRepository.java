4fcda21dc67aa860658015850c0313e68b7f2eb6
TiagoBorja <borjatiago05@gmail.com>
2024-08-16 00:46:51 +0100

    Repository - ReviewRepository

---
+++ bookreview/src/main/java/com/tiagoborja/bookreview/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.tiagoborja.bookreview.repository;
+
+import com.tiagoborja.bookreview.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Long, Review> {
+}
