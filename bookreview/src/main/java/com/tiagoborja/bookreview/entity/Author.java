2c5f6710ba8a697acfab80262546af458219a507
TiagoBorja <borjatiago05@gmail.com>
2024-08-16 00:37:44 +0100

    Entity - Author

---
+++ bookreview/src/main/java/com/tiagoborja/bookreview/entity/Author.java
@@ -0,0 +1,43 @@
+package com.tiagoborja.bookreview.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "bio", columnDefinition = "TEXT")
+    private String bio;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+}
