cb6709e1e45a34068570aeae0f02340f5076e553
TiagoBorja <borjatiago05@gmail.com>
2024-08-16 00:47:08 +0100

    Repository - ReviewCommentRepository

---
+++ bookreview/src/main/java/com/tiagoborja/bookreview/repository/ReviewCommentRepository.java
@@ -0,0 +1,7 @@
+package com.tiagoborja.bookreview.repository;
+
+import com.tiagoborja.bookreview.entity.ReviewComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewCommentRepository extends JpaRepository<Long, ReviewComment> {
+}
