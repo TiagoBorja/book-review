ab121f01c79bee401665add29575cc540bfc2c22
TiagoBorja <borjatiago05@gmail.com>
2024-08-16 01:12:06 +0100

    Service - AuthorService | Refactor - AuthorRepositoy

--- bookreview/src/main/java/com/tiagoborja/bookreview/repository/AuthorRepository.java
+++ bookreview/src/main/java/com/tiagoborja/bookreview/repository/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.tiagoborja.bookreview.repository;
 
 import com.tiagoborja.bookreview.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthorRepository extends JpaRepository<Long, Author> {
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
---
+++ bookreview/src/main/java/com/tiagoborja/bookreview/service/AuthorService.java
@@ -0,0 +1,48 @@
+package com.tiagoborja.bookreview.service;
+
+import com.tiagoborja.bookreview.entity.Author;
+import com.tiagoborja.bookreview.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author getAuthorById(Long id) {
+        return authorRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found"));
+    }
+
+    public Author createAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public Author updateAuthor(Long id, Author authorDetails) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found"));
+
+        author.setName(authorDetails.getName());
+        author.setBio(authorDetails.getBio());
+
+        return authorRepository.save(author);
+    }
+
+    public void deleteAuthor(Long id) {
+        if (!authorRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found");
+        }
+        authorRepository.deleteById(id);
+    }
+}
