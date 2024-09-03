5e5f46baabb5ccaa5754c018cc5021db9c9ea44f
TiagoBorja <borjatiago05@gmail.com>
2024-09-03 17:35:51 +0100

    Exclude ReviwComment

--- bookreview/src/main/java/com/tiagoborja/bookreview/repository/ReviewCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tiagoborja.bookreview.repository;
-
-import com.tiagoborja.bookreview.entity.ReviewComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Long> {
-}
